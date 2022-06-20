851cb85e2c73be2045701ff10630ba3302e365fd
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-06-20 11:59:10 -0700

    added nav bar, header and login form, errors with hitting backend api to log in

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
+import Link from 'next/link';
+import { AuthProvider } from '../contexts/auth';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <header className="flex">
+    <div className="w-full border-2 border-violet-900 bg-violet-300">
+    <h1 className="text-4xl text-center"> Welcome to Smells Like Devs Cooking</h1>
+      <nav className="flex items-center gap-10 pt-5 justify-center">
+        <Link href="/"><a>Home</a></Link>
+        <Link href="/about"><a>About the Cooks</a></Link>
+        <Link href="/create"><a>Create a Blog Post</a></Link>
+        <Link href="/owner"><a>Profile</a></Link>
+      </nav>
+    </div>
+  </header>
+  <AuthProvider>
+    <Component {...pageProps} />
+  </AuthProvider>
+  </>
+  )
 }
 
 export default MyApp
---
+++ pages/create.js
@@ -0,0 +1,7 @@
+export default function Home() { 
+	return ( 
+		<h1 className="text-3xl font-bold underline">
+			Ideally this is gonna link to a form :D 
+		</h1> 
+		) 
+	}
