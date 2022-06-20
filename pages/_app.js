15e3b7c54d131df22360305c2c0e28bce6e8c40d
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-06-20 14:10:03 -0700

    added a footer and login works

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,10 @@ function MyApp({ Component, pageProps }) {
   <AuthProvider>
     <Component {...pageProps} />
   </AuthProvider>
+  <footer className="w-full border-2 border-violet-900 bg-violet-300">
+    <p >&copy;2022</p>
+    <a href="https://github.com/The-Spice-Devs">The Spice Devs GitHub</a>
+  </footer>
   </>
   )
 }
