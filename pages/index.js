af1f8ea9bc4a5ac3c5c0ff2be142f175931fbcbb
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-06-20 20:26:59 -0400

    added form to create user

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 import Main from '../components/Main';
 import LoginForm from '../components/LoginForm';
 import { useAuth } from '../contexts/auth'
+import CreateUserForm from './createUserFormPage';
 
 export default function Home() { 
 	
 	const { user, login, logout } = useAuth();
 
-	if (!user) return <LoginForm login={login} />
+	if (!user) return (
+		<div>
+			{/* <LoginForm login={login} /> */}
+			<br></br>
+			<CreateUserForm />
+		</div>
+	)
 
 	return ( 
+		<div>
 		<Main logout={logout} username={user.username}/>
+		</div>
 		) 
 	}
 
