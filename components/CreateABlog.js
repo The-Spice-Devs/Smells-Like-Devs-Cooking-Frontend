f4c7dab0b8a15882da57b2f7c6619c90f37c721c
Thomas Basham <Bashamtg@gmail.com>
2022-06-22 12:13:02 -0700

    user image upload function working

--- components/CreateABlog.js
+++ components/CreateABlog.js
@@ -1,15 +1,44 @@
 import { useAuth } from "../contexts/auth";
+import React, { useState } from "react";
+import axios from "axios";
+import useResource from "../hooks/useResource";
 
-export default function CreateBlogPage({createResource}){
+
+export default function CreateBlogPage({ createResource }) {
+  const cloudinaryURL = process.env.NEXT_PUBLIC_RESOURCE_CLOUD_URL;
   const { user } = useAuth();
-  console.log(user)
-  function formHandler(event) {
+
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+
+  console.log("URL: ", url);
+
+  console.log("USER: ", user);
+
+  
+
+  async function formHandler(event) {
     event.preventDefault();
+
+    // Get image data from form, add Cloudinary data
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "recipe_images");
+    data.append("cloud_name", "dkgm8e6lz");
+
+    axios.post(cloudinaryURL, data)
+      .then((res) => res.data)
+      .then((data) => {
+        setUrl(data.url);
+      })
+
+      .catch((err) => console.log(err));
+
     let newBlog = {
       title: event.target.title.value,
       recipe_ingredients: createIngredientList(event),
       recipe_directions: createDirectionList(event),
-      recipe_image: event.target.recipe_image.value,
+      recipe_images: url,
       difficulty: event.target.difficulty.value,
       recipe_intro: event.target.recipe_intro.value,
       prep_time: event.target.prep_time.value,
@@ -20,55 +49,126 @@ export default function CreateBlogPage({createResource}){
       owner: user.id,
     };
 
-    function createIngredientList(event){
+    function createIngredientList(event) {
       let ingredients = event.target.recipe_ingredients.value.split(",");
-      for(let i = 0; i < ingredients.length; i++){
+      for (let i = 0; i < ingredients.length; i++) {
         ingredients[i] = ingredients[i].replace(/^\s*/, "").replace(/\s*$/, "");
       }
-      return ingredients
+      return ingredients;
     }
-    function createDirectionList(event){
+    function createDirectionList(event) {
       let directions = event.target.recipe_directions.value.split(",");
-      return directions
+      return directions;
     }
-    createResource(newBlog);
+
+    if (url.length > 5) {
+      createResource(newBlog);
+    }
   }
 
-  return(
-    <form onSubmit={formHandler} method="post" className="justify-center w-full h-full max-w-screen-xl px-5 py-3 ml-auto mr-auto text-center border-4 border-solid rounded-lg top-56 bg-violet-200 border-violet-500 my-5">
-    <div className="flex flex-col p-4">
-      <h1 className="text-xl py-4 font-bold">Create A New Blog Post!</h1>
-    <label htmlFor="title" className="py-4 font-bold text-2xl">Recipe Title</label>
-    <input placeholder="Type Title Of Recipe!" type="text" id="title" name="title"></input>
+  
+  return (
+    <form
+      onSubmit={formHandler}
+      method="post"
+      className="justify-center w-full h-full max-w-screen-xl px-5 py-3 my-5 ml-auto mr-auto text-center border-4 border-solid rounded-lg top-56 bg-violet-200 border-violet-500"
+    >
+      <div className="flex flex-col p-4">
+        <h1 className="py-4 text-xl font-bold">Create A New Blog Post!</h1>
+        <label htmlFor="title" className="py-4 text-2xl font-bold">
+          Recipe Title
+        </label>
+        <input
+          placeholder="Type Title Of Recipe!"
+          type="text"
+          id="title"
+          name="title"
+        ></input>
 
+        <label htmlFor="recipe_intro" className="py-4 text-2xl font-bold">
+          About the Recipe
+        </label>
+        <input
+          placeholder="Tell us about the recipe!"
+          type="text"
+          id="recipe_intro"
+          name="recipe_intro"
+        />
 
-    <label htmlFor="recipe_intro" className="py-4 font-bold text-2xl">About the Recipe</label>
-    <input placeholder="Tell us about the recipe!" type="text" id="recipe_intro" name="recipe_intro" />
+        <label htmlFor="recipe_images" className="py-4 text-2xl font-bold">
+          Recipe Image
+        </label>
+        {/* <input placeholder="input url for image"type="text" id="recipe_images" name="recipe_images" /> */}
+        <input
+          type="file"
+          onChange={(e) => setImage(e.target.files[0])}
+        ></input>
 
-    <label htmlFor="recipe_image" className="py-4 font-bold text-2xl">Recipe Image</label>
-    <input placeholder="input url for image"type="text" id="recipe_image" name="recipe_image" />
+        <label htmlFor="recipe_ingredients" className="py-4 text-2xl font-bold">
+          Recipe Ingredients
+        </label>
+        <input
+          placeholder="input recipe ingredients with commas separating each ingredient (ex: potatoes, butter, milk)"
+          id="recipe_ingredients"
+          name="recipe_ingredients"
+        />
 
-    <label htmlFor="recipe_ingredients" className="py-4 font-bold text-2xl">Recipe Ingredients</label>
-    <input placeholder="input recipe ingredients with commas separating each ingredient (ex: potatoes, butter, milk)" id="recipe_ingredients" name="recipe_ingredients" />
+        <label htmlFor="recipe_directions" className="py-4 text-2xl font-bold">
+          Recipe Directions
+        </label>
+        <input
+          placeholder="input recipe directions with commas separating each direction (ex:  boil potatoes, add butter to potatoes, add milk to potatoes, mix)"
+          id="recipe_directions"
+          name="recipe_directions"
+        />
 
-    <label htmlFor="recipe_directions" className="py-4 font-bold text-2xl">Recipe Directions</label>
-    <input placeholder="input recipe directions with commas separating each direction (ex:  boil potatoes, add butter to potatoes, add milk to potatoes, mix)" id="recipe_directions" name="recipe_directions" />
+        <label htmlFor="meal_type" className="py-4 text-2xl font-bold">
+          Meal Type
+        </label>
+        <input
+          placeholder="Input Meal type (ex: Dinner, Breakfast, Snack etc)"
+          type="text"
+          id="meal_type"
+          name="meal_type"
+        />
 
-    <label htmlFor="meal_type" className="py-4 font-bold text-2xl">Meal Type</label>
-    <input placeholder="Input Meal type (ex: Dinner, Breakfast, Snack etc)"type="text" id="meal_type" name="meal_type" />
+        <label htmlFor="prep_time" className="py-4 text-2xl font-bold">
+          Prep Time
+        </label>
+        <input
+          placeholder="Input a number of minutes"
+          type="number"
+          id="prep_time"
+          name="prep_time"
+        />
 
-    <label htmlFor="prep_time" className="py-4 font-bold text-2xl">Prep Time</label>
-    <input placeholder= "Input a number of minutes" type="number" id="prep_time" name="prep_time" />
+        <label htmlFor="cook_time" className="py-4 text-2xl font-bold">
+          Cook Time
+        </label>
+        <input
+          placeholder="Input a number of minutes"
+          type="number"
+          id="cook_time"
+          name="cook_time"
+        />
 
-    <label htmlFor="cook_time" className="py-4 font-bold text-2xl">Cook Time</label>
-    <input placeholder= "Input a number of minutes" type="number" id="cook_time" name="cook_time" />
+        <label htmlFor="difficulty" className="py-4 text-2xl font-bold">
+          Difficulty between 1 and 5
+        </label>
+        <input
+          placeholder="Input a number between 1 and 5"
+          type="number"
+          id="difficulty"
+          name="difficulty"
+        />
 
-    <label htmlFor="difficulty" className="py-4 font-bold text-2xl">Difficulty between 1 and 5</label>
-    <input placeholder="Input a number between 1 and 5" type="number" id="difficulty" name="difficulty" />
-
-
-    <button type="submit" className="py-4 mt-4 rounded p bg-violet-500 font-bold">Submit</button>
-  </div>
-</form >
-  )
+        <button
+          type="submit"
+          className="py-4 mt-4 font-bold rounded p bg-violet-500"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
 }
