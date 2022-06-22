97e125b83681903090467ca58a5b140574ce219a
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-06-21 17:09:12 -0700

    added Profile page rendering to page

--- pages/owner.js
+++ pages/owner.js
@@ -1,8 +1,21 @@
-export default function Home() { 
-	return ( 
-		<h1 className="text-3xl font-bold underline">
-			Some neat cool profile stuff here 
-		</h1> 
-		) 
-	}
+import { useRouter } from 'next/router'
+import { useAuth } from '../contexts/auth'
+import { useEffect } from 'react'
+import ProfilePage from '../components/ProfilePage';
 
+
+export default function User() {
+    const { user } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!user) {
+            router.push('/')
+        }
+    })
+
+
+    return (
+			<ProfilePage />
+		)
+}
