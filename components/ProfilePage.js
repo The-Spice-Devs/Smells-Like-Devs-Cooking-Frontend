528f25f0c6503c2e696540da4890f69ed0a55484
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-06-22 21:09:57 -0700

    added style, about to have some conflict with Thomas

--- components/ProfilePage.js
+++ components/ProfilePage.js
@@ -2,72 +2,61 @@ import useResource from '../hooks/useResource';
 import { useAuth } from '../contexts/auth'
 import Link from "next/link"
 import Header from './Header';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
 import banner from '../assets/profileBanner.jpeg'
 
-export default function Main({ username, logout }) {
+export default function Main() {
 
   const { resources } = useResource();
   const { user } = useAuth();
-  const router = useRouter();
 
   if (!resources) return <h2>No Blog Posts Available, Try again later! :)</h2>
 
-  // if (!user){
-  //   router.push('/');
-  // }
-
   return (
     <>
-    <Header />
-    {/* ternary: if no user, this page just returns a header, and goes to home route, because of logic on owner.js */}
-    <div className="pt-2" style={{ position: 'relative', width: '100vw', height: '25vw' }}>
-      <h1 className="text-8xl z-10 font-Rampart text-center" style={{ position: 'absolute', top: '30%',left:'15%', opacity: '1'}}>Smells Like Devs Cooking</h1>
+      <Header />
+      {/* ternary: if no user, this page just returns a header, and goes to home route, because of logic on owner.js */}
+      <div className="pt-2" style={{ position: 'relative', width: '100vw', height: '20vw' }}>
+        <h1 className="text-7xl z-10 font-MajorMono text-center" style={{ position: 'absolute', top: '30%', left: '15%', opacity: '1', color: 'rgb(41,0,0)'}}>Smells Like Devs Cooking</h1>
         <Image
           src={banner}
           layout="fill"
           objectFit="cover"
-          alt="Yhe SPice Devs"
-          style={{opacity:'0.7'}}
+          alt="Profile Page Banner"
+          style={{ opacity: '0.6', zIndex: '-1' }}
+          priority
         />
-        </div>
+      </div>
 
-     {user &&   <>     
-     <h1>Welcome to Your Profile {user.username}</h1>
-    <h2> Here are your create recipes:</h2>
-      <div className="flex">
+      {user && <>
+        <div className="items-center justify-center gap-4 p-8 mx-auto my-7 text-center bg-orange-200 border-2 border-orange-400 rounded-lg text-md gap-x-8 w-4/5">
+        <h1 className="text-amber-900 text-5xl pb-10 font-bold">Welcome to Your Profile {user.username}!</h1>
+        <h2 className="text-amber-900 text-4xl pb-10 font-bold"> Here are the Recipes you have Created:</h2>
+      </div>
+        <div className="flex flex-wrap w-3/4 justify-center">
         {resources.map((blog) => {
           return (
-            <div className="border-2 border-violet-500 bg-violet-300 my-10 mx-3 pl-3 w-1/4 text-center" key={blog.id}>
-              <Link href={`/${blog.id}`}><a className="text-2xl font-bold underline">{blog.title}</a></Link>
-              <p className="italic">Author: {blog.owner} Date Created: {blog.created_at}</p>
-              <p className="py-1">Meal Type: {blog.meal_type}</p>
-              <p className="py-1">Level of Difficulty: {blog.difficulty} out of 5</p>
-              <p className="py-1"> Image {blog.recipe_images}</p>
-              <p className="py-1 italic">Ratings Coming Soon!</p>
-              <p className="py-1 italic">Dietary Tags Coming Soon!</p>
-            </div>
-          )
-        })}
-        </div>
-        <h2> These are the recipes you&apos;ve favorited:</h2>
-        <div className="flex">
-        {resources.map((blog) => {
-          return (
-            <div className="border-2 border-violet-500 bg-violet-300 my-10 mx-3 pl-3 w-1/4 text-center" key={blog.id}>
-              <Link href={`/${blog.id}`}><a className="text-2xl font-bold underline">{blog.title}</a></Link>
-              <p className="italic">Author: {blog.owner} Date Created: {blog.created_at}</p>
-              <p className="py-1">Meal Type: {blog.meal_type}</p>
-              <p className="py-1">Level of Difficulty: {blog.difficulty} out of 5</p>
-              <p className="py-1"> Image {blog.recipe_images}</p>
-              <p className="py-1 italic">Ratings Coming Soon!</p>
-              <p className="py-1 italic">Dietary Tags Coming Soon!</p>
+            <div className='shadow-2xl w-80 shadow-amber-500 mx-7 my-10' style={{color: 'rgb(41,0,0)'}} key={blog.id}>
+              <Link href={`/${blog.id}`}><a className="text-2xl w-250 flex justify-center font-bold underline pb-5" >{blog.title}</a></Link>
+                <Image src={blog.recipe_images} alt={blog.title} width={320} height={320} />
             </div>
           )
         })}
       </div>
-      </>} 
+      <div className="items-center justify-center gap-4 p-8 mx-auto my-7 text-center bg-orange-200 border-2 border-orange-400 rounded-lg text-md gap-x-8 w-4/5">
+        <h2 className="text-amber-900 text-4xl pb-10 font-bold"> Here are the Recipes you have Favorited:</h2>
+      </div>
+        <div className="flex flex-wrap w-3/4 justify-center">
+        {resources.map((blog) => {
+          return (
+            <div className='shadow-2xl w-80 shadow-amber-500 mx-7 my-10' style={{color: 'rgb(41,0,0)'}} key={blog.id}>
+              <Link href={`/${blog.id}`}><a className="text-2xl w-250 flex justify-center font-bold underline pb-5" >{blog.title}</a></Link>
+                <Image src={blog.recipe_images} alt={blog.title} width={320} height={320} />
+            </div>
+          )
+        })}
+      </div>
+      </>}
     </>
   )
 }
